2b34d4c64b98d82f8f7409ed9a92c3c50b96f311
vinodkumar17-8-6 <73932115+vinodkumar17-8-6@users.noreply.github.com>
2020-12-09 03:06:28 -0500

    Add files via upload

---
+++ Blocks.js
@@ -0,0 +1,42 @@
+class Blocks {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution': 1.0,
+          'friction' : 0.8,
+          'density': 0.3
+      }
+      this.visibility = 255;
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      
+      if(this.body.speed <4){
+        var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("green");
+      rect(0,0, this.width, this.height);
+      pop();
+      }
+       else{
+        World.remove(world, this.body);
+        push();
+            this.Visiblity = this.Visiblity - 5;
+            tint(255,this.Visiblity);
+            pop();
+        }
+      }
+      
+      score(){
+        if (this.Visiblity < 0 && this.Visiblity > -1005){
+          score++;
+        }
+      }
+  };
